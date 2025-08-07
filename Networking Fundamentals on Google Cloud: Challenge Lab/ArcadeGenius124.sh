e9b266dda3cb6576fe6cdf32c4d3fb0b4ee024af
Genius <arcadegenuis@gmail.com>
2025-08-07 14:23:56 +0530

    ArcadeGenius124.sh

--- Networking Fundamentals on Google Cloud: Challenge Lab/ArcadeGenius124.sh
+++ Networking Fundamentals on Google Cloud: Challenge Lab/ArcadeGenius124.sh
@@ -1,10 +1,9 @@
-
-
+```bash
 gcloud auth list
 
 export REGION=${ZONE%-*}
 
-
+# Create Apache Web Servers
 gcloud compute instances create web1 --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-small --tags=network-lb-tag --image-family=debian-11 --image-project=debian-cloud \
 --metadata=startup-script='#!/bin/bash
 apt-get update
@@ -26,33 +25,27 @@ apt-get install apache2 -y
 service apache2 restart
 echo "<h3>Web Server: web3</h3>" | tee /var/www/html/index.html'
 
-
-
+# Firewall Rule
 gcloud compute firewall-rules create www-firewall-network-lb --allow tcp:80 --target-tags network-lb-tag
 
-
-
+# Create Static IP
 gcloud compute addresses create network-lb-ip-1 --region=$REGION  
 
-
+# Health Check
 gcloud compute http-health-checks create basic-check
 
-
- gcloud compute target-pools create www-pool --region=$REGION  --http-health-check basic-check
-
+# Target Pool
+gcloud compute target-pools create www-pool --region=$REGION  --http-health-check basic-check
 
 gcloud compute target-pools add-instances www-pool --instances web1,web2,web3 --zone=$ZONE
-    
 
-
+# Forwarding Rule
 gcloud compute forwarding-rules create www-rule --region=$REGION --ports 80 --address network-lb-ip-1 --target-pool www-pool
 
-
-
+# Get IP Address
 IPADDRESS=$(gcloud compute forwarding-rules describe www-rule --region=$REGION  --format="json" | jq -r .IPAddress)
 
-
-
+# Create Instance Template for Backend Group
 gcloud compute instance-templates create lb-backend-template --project=$DEVSHELL_PROJECT_ID --region=$REGION --network=default --subnet=default --tags=allow-health-check --machine-type=e2-medium --image-family=debian-11 --image-project=debian-cloud --metadata=startup-script='#!/bin/bash
      apt-get update
      apt-get install apache2 -y
@@ -64,33 +57,29 @@ gcloud compute instance-templates create lb-backend-template --project=$DEVSHELL
      tee /var/www/html/index.html
      systemctl restart apache2'
 
-
+# Managed Instance Group
 gcloud compute instance-groups managed create lb-backend-group --template=lb-backend-template --size=2 --zone=$ZONE 
 
-
+# Health Check Firewall
 gcloud compute firewall-rules create fw-allow-health-check --network=default --action=allow --direction=ingress --source-ranges=130.211.0.0/22,35.191.0.0/16 --target-tags=allow-health-check --rules=tcp:80
 
-
+# Global IPv4 Address
 gcloud compute addresses create lb-ipv4-1 --ip-version=IPV4 --global
-
 
 gcloud compute addresses describe lb-ipv4-1 --format="get(address)" --global
 
-
+# Global HTTP Load Balancer
 gcloud compute health-checks create http http-basic-check --port 80
-
 
 gcloud compute backend-services create web-backend-service --protocol=HTTP --port-name=http --health-checks=http-basic-check --global
 
-
 gcloud compute backend-services add-backend web-backend-service --instance-group=lb-backend-group --instance-group-zone=$ZONE --global
-
 
 gcloud compute url-maps create web-map-http --default-service web-backend-service
 
-
 gcloud compute target-http-proxies create http-lb-proxy --url-map web-map-http
 
-
 gcloud compute forwarding-rules create http-content-rule --address=lb-ipv4-1 --global --target-http-proxy=http-lb-proxy --ports=80
+```
 
+> ✅ Don’t forget to subscribe to our YouTube Channel: [Arcade Genius](https://www.youtube.com/@ArcadeGenius-z1)
