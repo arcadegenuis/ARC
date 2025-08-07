7dbe4a2343a026b4d377b405e91489dabc6d5a4e
Genius <arcadegenuis@gmail.com>
2025-08-07 15:32:44 +0530

    ArcadeGenius234.sh

--- Arcade Hero: Enter the Cloud Function Ruby/ArcadeGenius234.sh
+++ Arcade Hero: Enter the Cloud Function Ruby/ArcadeGenius234.sh
@@ -1,19 +1,22 @@
 
+```bash
 gcloud auth list
 
 gcloud config set compute/region $REGION
 
 export PROJECT_ID=$DEVSHELL_PROJECT_ID
 
-mkdir techcps && cd techcps
+mkdir ArcadeGenius && cd ArcadeGenius
+```
 
-
+```ruby
 cat > Gemfile <<'EOF_CP'
 source "https://rubygems.org"
 gem "functions_framework", "~> 0.7"
 EOF_CP
+```
 
-
+```ruby
 cat > Gemfile.lock <<'EOF_CP'
 GEM
   remote: https://rubygems.org/
@@ -38,7 +41,9 @@ DEPENDENCIES
 BUNDLED WITH
    2.4.6
 EOF_CP
+```
 
+```ruby
 cat > app.rb <<'EOF_CP'
 require "functions_framework"
 require "cgi"
@@ -53,16 +58,19 @@ FunctionsFramework.http "hello_http" do |request|
   "Hello #{CGI.escape_html name}!"
 end
 EOF_CP
+```
 
+```bash
 bundle install
 
-
 sleep 15
+```
 
+```bash
 #!/bin/bash
 
 deploy_function() {
-    gcloud functions deploy cf-demo \
+  gcloud functions deploy cf-demo \
     --gen2 \
     --runtime=ruby33 \
     --entry-point=hello_http \
@@ -70,22 +78,21 @@ deploy_function() {
     --region=$REGION \
     --trigger-http \
     --allow-unauthenticated --quiet
-  }
-  
-  deploy_success=false
-  
-  while [ "$deploy_success" = false ]; do
-    if deploy_function; then
-      echo "Function deployed successfully (https://www.youtube.com/@techcps).."
-      deploy_success=true
-    else
-      echo "please subscribe to techcps (https://www.youtube.com/@techcps)."
-      sleep 10
-    fi
-  done  
-  
-  
-  echo "Congratulations, you're all done with the lab"
-  echo "Please like share and subscribe to techcps(https://www.youtube.com/@techcps)..."
-  
-  
+}
+
+deploy_success=false
+
+while [ "$deploy_success" = false ]; do
+  if deploy_function; then
+    echo "Function deployed successfully (https://www.youtube.com/@ArcadeGenius-z1).."
+    deploy_success=true
+  else
+    echo "please subscribe to Arcade Genius (https://www.youtube.com/@ArcadeGenius-z1)."
+    sleep 10
+  fi
+done  
+
+echo "Congratulations, you're all done with the lab"
+echo "Please like, share, and subscribe to Arcade Genius (https://www.youtube.com/@ArcadeGenius-z1)..."
+```
+
